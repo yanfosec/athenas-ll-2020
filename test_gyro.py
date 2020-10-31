68ce10b290d5502f490417ee560178f4941e5ef9
Yan Kravchenko <yan.kravchenko@gmail.com>
2020-10-30 19:46:00 -0500

    Update test_gyro.py
    Gyro Trials!

--- test_gyro.py
+++ test_gyro.py
@@ -8,13 +8,28 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from linefollow import followBlack
 from turning import calcOffset, turnBot
+import time
 
 #######################################################################
 ## Run Code Starts Here                                              ##
 #######################################################################
 
 #print(str(calcOffset(gyro)))
+print("Gyro Turns")
+for x in range(10):
+    turnBot(gyro,'R', 89)
+    turnBot(gyro,'L', 89)
 
-turnBot(gyro,'R', 89)
-turnBot(gyro,'L', 89)
-
+# time.sleep(1)
+# print("Driver Turns")
+# for y in range(10):
+#     gyro.reset_angle(0)
+#     driver.turn(90)
+#     print(str(gyro.angle()))
+#     driver.stop()
+#     time.sleep(1)
+#     gyro.reset_angle(0)
+#     driver.turn(-90)
+#     print(str(gyro.angle()))
+#     driver.stop()
+#     time.sleep(1)
