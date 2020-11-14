95ea6b15d95dee9be26ca790b92eb5e56d6c40ff
Joe A May <may.joe.a@gmail.com>
2020-11-14 14:23:11 -0600

    R2 Successful
    Dumping bricks, deploying Boccia and navigating back to home.  65-75 points, depending on if the yellow brick goes in.  Not yet picking up health unit consistently.

--- r2_nico.py
+++ r2_nico.py
@@ -7,22 +7,45 @@ import time
 from turning import gyroRight,gyroLeft
 from dump import Dump
 
+driver.settings(straight_speed=250)
+
 #drive to the dance floor
-gyroRight(30)
+gyroRight(28)
 driver.straight(935)
 driver.stop()
 gyroRight(65)
+driver.settings(straight_speed=125)
 while r_color.reflection() >12:
     driver.drive(125, 0)
 driver.stop()
 driver.straight(20)
 driver.stop()
-while r_color.reflection() >12:
-    driver.drive(125, 0)
+#while r_color.reflection() >12:
+ #   driver.drive(125, 0)
+driver.straight(125)
 driver.stop()
-gyroRight(60)
+gyroRight(65)
 driver.stop()
-dumper.down(100,100)
-
-
+driver.straight(-50)
+dumper.down(300,90)
+driver.straight(10)
+dumper.up(300,30)
+driver.straight(125)
+driver.stop()
+gyroRight(40)
+driver.straight(-55)
+driver.straight(10)
+dumper.up(150,10)
+driver.straight(10)
+dumper.up(150,10)
+driver.straight(50)
+driver.stop()
+driver.settings(straight_speed=300)
+driver.stop()
+gyroRight(55)
+driver.straight(500)
+driver.stop()
+gyroLeft(65)
+driver.straight (800)
+dumper.up(300,40)
 
