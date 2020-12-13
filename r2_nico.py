d45cdf3dd8fe23f7d988e79f23af7e990b0c82a7
Joe A May <may.joe.a@gmail.com>
2020-12-13 09:19:00 -0600

    12/12 Meeting Updates
    Main module that uses buttons to control launch.  Updates to run 3 using new passive attachment.

--- r2_nico.py
+++ r2_nico.py
@@ -7,56 +7,61 @@ import time
 from turning import gyroRight,gyroLeft
 from dump import Dump
 
-driver.settings(straight_speed=250)
-
-#drive to the dance floor
-door.up(150,50)#lifts door uo 50%
-gyroRight(28)#turns right to face dance floor health
-driver.straight(935) #drives to dance floor
-driver.stop()
-door.down(150,50)
-driver.stop() #stops for gyro
-gyroRight(65) #turns to face black line
-driver.settings(straight_speed=125) #set straight speed
-while r_color.reflection() >12: #goes to black line 
-    driver.drive(125, 0)
-driver.stop() #stops
-driver.straight(145) #moves stright to get around target
-driver.stop()#stops for gyro
-gyroRight(65)#turns to get dump facting target
-driver.stop() #stops
-driver.straight(-25) #moves backwards to get dumper over target
-driver.stop()
-dumper.down(300,90) #moves dumper down 90% to dump blocks
-driver.straight(10) #move forward to not catch on target
-driver.stop()
-dumper.up(300,30) #dumper moves up 30% to get lined up with flip
-driver.straight(125) #moves forward to be at right distance to flip
-driver.stop() #stops for gyro
-gyroRight(40) #turns to line up wiht flip
-driver.straight(-55) #backward so flip is in dump and turn flip
-driver.straight(10) #forward for +special effects+ (also so we knock flip sligly)
-dumper.up(150,10) #dump moves up to flip
-driver.straight(10) #forward to not catch
-dumper.up(150,10) #moves up to finish fliping the flip
-driver.straight(50) #goes forward to 
-driver.stop()
-driver.settings(straight_speed=300)
-driver.stop() 
-gyroRight(20)
-driver.stop()
-driver.straight(50)
-door.up(125,45)
-driver.stop()
-driver.straight(50)
-driver.stop()
-driver.straight(-50)
-driver.stop()
-door.down(100,45)
-gyroRight(41)
-driver.straight(1300)
-#driver.stop()
-#gyroLeft(65)
-#driver.straight (800)
-#dumper.up(300,40)
-
+def run2():
+    driver.settings(straight_speed=300)
+    #drive to the dance floor
+    door.up(150,50)#lifts door uo 50%
+    gyroRight(28)#turns right to face dance floor health
+    driver.straight(935) #drives to dance floor
+    driver.stop()
+    door.down(150,50)
+    driver.stop() #stops for gyro
+    gyroRight(65) #turns to face black line
+    driver.settings(straight_speed=125) #set straight speed
+    while r_color.reflection() >12: #goes to black line 
+        driver.drive(125, 0)
+    driver.stop() #stops
+    driver.straight(145) #moves stright to get around target
+    driver.stop()#stops for gyro
+    gyroRight(65)#turns to get dump facting target
+    driver.stop() #stops
+    driver.straight(-25) #moves backwards to get dumper over target
+    driver.stop()
+    dumper.down(350,95) #moves dumper down 90% to dump blocks
+    driver.straight(10) #move forward to not catch on target
+    driver.stop()
+    dumper.up(300,30) #dumper moves up 30% to get lined up with flip
+    driver.straight(125) #moves forward to be at right distance to flip
+    driver.stop() #stops for gyro
+    gyroRight(40) #turns to line up wiht flip
+    driver.straight(-55) #backward so flip is in dump and turn flip
+    driver.straight(10) #forward for +special effects+ (also so we knock flip sligly)
+    dumper.up(150,10) #dump moves up to flip
+    driver.straight(10) #forward to not catch
+    dumper.up(150,10) #moves up to finish fliping the flip
+    driver.straight(50) #goes forward to 
+    driver.stop()#
+    driver.settings(straight_speed=300)
+    driver.stop() 
+    gyroRight(20)
+    driver.stop()
+    driver.straight(50)
+    door.up(125,45)
+    driver.stop()
+    driver.settings(straight_speed=300)
+    driver.straight(70)
+    driver.stop()
+    driver.straight(-72)
+    driver.stop()
+    door.down(100,45)
+    dumper.up(300,40)
+    gyroRight(41)
+    driver.straight(400)
+    driver.stop()
+    gyroLeft(95)
+    door.up(100,80)
+    driver.straight (496)
+    driver.stop()
+    door.down(100,75)
+    gyroRight(90)
+    driver.straight(803)
--- r3_Mal.py
+++ r3_Mal.py
@@ -1,34 +1,16 @@
 #!/usr/bin/env pybricks-micropython
-
-#import codes
 from robotsetup import ev3, driver, door, r_color, l_color
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
-from linefollow import followBlack
+from turning import gyroRight, gyroLeft
 
-driver.settings(straight_speed=200) #setting the speed when you go straight to always be 200
-
-#drive to bench
-driver.straight(450) #goes straight to bench driving health units and innovation challenge
-driver.stop() #stops 
-driver.straight(-40) #driver moves back slightly back as to not hit innovation challenge and health
-driver.stop() #stops
-driver.turn(-25) #turn and knock the bench down
-driver.straight(-230)#backup and turn to face bench
-driver.stop()#stops
-driver.turn(-80) #turns to be almost parallel to the bench
-driver.straight(300) #moves to be around bench
-driver.stop() #stops
-driver.turn(110) #turns to face bench
-driver.straight(350) #drives straight to bench. use bench to square up with handle
-driver.stop() #stops
-door.up(160,60) #forklift moves up to grab handle
-driver.straight(-500) #drive backwards towards home
-driver.stop() #Stops
-door.down(160,60)
-
-####YAYAYAYAYAYAY####
+def run3():
+    driver.settings(straight_speed=200) #setting the speed when you go straight to always be 200
+    driver.straight(445) #goes straight to bench driving health units and innovation challenge
+    driver.stop() #stops
+    driver.straight(-20)
+    driver.stop()
+    door.up(200,10)
+    driver.stop()
+    gyroRight(15)
+    driver.straight(-450)
+    door.down(200,10)
+    driver.stop()
